c1c8506fb72763c1ebb0533ac180167557951932
Sylvain Hellegouarch <sh@defuze.org>
2025-09-05 17:27:31 +0200

    add httpx
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from typing import Union
 
+import httpx
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -11,6 +12,5 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
+    return httpx.get("https://www.google.com").text
 
